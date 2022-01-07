41e27b68a39f2cb351565a8044e73e913e26e1ad
Drew Speed <drewjspeed@gmail.com>
2022-01-06 22:05:39 -0600

    added charBank and character type confirmations

--- Develop/script.js
+++ Develop/script.js
@@ -12,19 +12,60 @@ function writePassword() {
   passwordText.value = password;
 
   // prompt for amount of characters requested between 8 and 128
+
+
   function passwordLength() {
-  var numberPrompt = window.prompt(
+  numberPrompt = window.prompt(
     "Please enter the amount of characters you would like in your password between 8 and 128.");
     // make numberprompt an integer
-    var numberPrompt = Math.floor(parseInt(numberPrompt));
+    numberPrompt = Math.floor(parseInt(numberPrompt));
     if (numberPrompt < 8 || numberPrompt > 128 || isNaN(numberPrompt)) {
       window.alert("You did not pick a valid option. Please try again");
       passwordLength();
+    } else {
+      charSelect();
     }
   };
   passwordLength();
-    // prompts for including lowercase, uppercase, numberic, and special characters
 
+
+
+  function charSelect() {
+    //object with banks for differect character types
+    chars = {
+      upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      lower: "abcdefghijklmnopqrstuvwxyz",
+      num: "1234567890",
+      spec: " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '"'
+    }
+    // Empty character bank
+    charBank = "";
+    // prompts and statements adding chars to charBank
+    upperConf =   confirm("Would you like for the password to contain UPPERCASE letters?");
+    if (upperConf) {
+      charBank += chars.upper;
+    }
+
+    lowerConf =   confirm("Would you like for the password to contain LOWERCASE letters?");
+    if (lowerConf) {
+      charBank += chars.lower;
+    } 
+
+    numConf = confirm("Would you like for the password to contain NUMBERS?");
+    if (numConf) {
+      charBank += chars.num;
+    }
+    specConf = confirm("Would you like for the password to contain SPECIAL CHARACTERS?")
+    if (specConf) {
+      charBank += chars.spec;
+    }
+  }
+
+
+  console.log(numberPrompt);
+  //function characterSelector(); {
+    // object with variables for different character types
+  //}
 
 }
 
